1cde06c97ffd27ac40a532f5f433f7de1ceba0e4
Andrii <el.vektor28@gmail.com>
2026-09-17 09:52:10 +0000

    task2: implement single parameter execution and directory status checks

--- Task2Shell/git_init.sh
+++ Task2Shell/git_init.sh
@@ -9,23 +9,57 @@ startup_security "$@"
 source "$CONFIG_PATH"
 confVar_security
 
+check_target_dir "$1"
+case "$?" in
+1|3)
+	if [[ $? -eq 1 ]];then
+        echo "Directory $1 is created"
+        fi
+	mkdir -p "$1"
 
-git init -b "$USER_BRANCH"
-git config --local user.name "$USER_NAME"
-git config --local user.email "$USER_EMAIL"
-git config --local init.defaultBranch "$USER_BRANCH"
-#git remote add origin "$USER_REMOTE"
+	clean_name="${1%/}"
+	dir_name="${clean_name##*/}"	
+	echo "# $dir_name" > "$clean_name/README.md"
+	echo "README.md created in $1"
 
-verify_cred
+	git -C "$1" init -b "$USER_BRANCH"
+	git -C "$1" config --local user.name "$USER_NAME"
+	git -C "$1" config --local user.email "$USER_EMAIL"
+	git -C "$1" config --local init.defaultBranch "$USER_BRANCH"
+	
+	if [[ -n "$2" ]];then
+	git -C "$1" remote add origin "$2"
+	fi
+	;;
+2)	
+	if [[ -z "$2" ]]; then
+	echo "To set remote, use the [remote_url] argument"
+	exit 0
+	else
+	echo "Adding remote origin to existing repository"
+	git -C "$1" remote add origin "$2"
+	fi
+	;;
+	
+esac
+
+verify_cred "$@"
 #rm -rf ./.git
 }
 
 verify_cred(){
 echo -e "\nParameters set as:"
-echo "NAME   = $(git config --local user.name)"
-echo "EMAIL  = $(git config --local user.email)"
-echo "BRANCH = $(git config --local init.defaultBranch)"
-#echo "REMOTE = $(git remote get-url origin)"
+echo "DIR=	$1"
+echo "NAME=	$(git -C "$1" config --local user.name)"
+echo "EMAIL=	$(git -C "$1" config --local user.email)"
+echo "BRANCH=	$(git -C "$1" config --local init.defaultBranch)"
+
+if git -C "$1" remote get-url origin >/dev/null 2>&1; then
+echo "REMOTE=	$(git -C "$1" remote get-url origin)"
+else
+echo "REMOTE=	NOT ASSIGNED"
+fi
+
 echo "==================="
 }
 
@@ -66,17 +100,6 @@ echo "Wrong argument value!"
 exit 1
 fi
 
-if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-echo "Repository is already initialized!"
-exit 2
-fi
-
-
-if [[ $# -eq 0 || "$1" == "-h" || "$1" == "--help" ]]; then
-show_help
-exit 0
-fi
-
 if [[ ! -f "$CONFIG_PATH" ]]; then
 echo "Config file does not exist!"
 ask_confirmation
@@ -84,6 +107,12 @@ while [ $? -eq 3 ]; do
 ask_confirmation
 done
 fi
+
+if [[ $# -eq 0 || "$1" == "-h" || "$1" == "--help" ]]; then
+show_help
+exit 0
+fi
+
 }
 
 confVar_security(){
@@ -118,7 +147,7 @@ ARGUMENTS:
 MODES:
 0 params	help
 1 params	create folder, git init, create README.md
-2 params	execute initialisation and link remote repository
+2 params	execute initialization and link remote repository
 
 EXAMPLES:
 ${0##*/} embedded_project
@@ -126,5 +155,27 @@ ${0##*/} driver_lib git@github.com:username/driver_lib.git
 EOF
 }
 
+check_target_dir(){
 
+if [[ ! -e "$1" ]]; then
+echo "Directory does not exist"
+return 1
+elif [[ -d "$1" ]];then
+	if git -C "$1" rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+	echo "Repository already initialized"
+	return 2
+	elif [[ -z "$(ls -A "$1")" ]];then
+	echo "Directory exists" 
+	return 3
+	else
+	echo "Directory is not empty!"
+	exit 6
+	fi
+	
+else
+echo "A file with this name already exists!"
+exit 5
+fi
+
+}
 main "$@"
